65d308275d8bbe4a7e155d622069eaa777c355da
omar@omarqureshi.net <omarqureshi@omarqureshi-VOSTRO.(none)>
2012-05-31 13:23:31 +0100

    Emacs 24 config

---
+++ init.el
@@ -0,0 +1,59 @@
+(require 'package)
+(add-to-list 'package-archives
+             '("marmalade" . "http://marmalade-repo.org/packages/") t)
+(package-initialize)
+
+;(let ((buffer (url-retrieve-synchronously
+;	       "http://tromey.com/elpa/package-install.el")))
+;  (save-excursion
+;    (set-buffer buffer)
+;    (goto-char (point-min))
+;    (re-search-forward "^$" nil 'move)
+;    (eval-region (point) (point-max))
+;    (kill-buffer (current-buffer))))
+
+(add-to-list 'load-path "~/.emacs.d/rhtml")
+(add-to-list 'load-path "~/.emacs.d/tomorrow-theme")
+(require 'rhtml-mode)
+(add-hook 'rhtml-mode-hook
+     	  (lambda () (rinari-launch)))
+
+;; visible bell
+(setq visible-bell nil)
+;; allow selection deletion
+(delete-selection-mode t)
+;; make sure delete key is delete key
+(global-set-key [delete] 'delete-char)
+;; turn on the menu bar
+(menu-bar-mode 1)
+;; have emacs scroll line-by-line
+(setq scroll-step 1)
+(require 'color-theme-tomorrow)
+(eval-after-load "color-theme"
+  '(progn
+     (color-theme-initialize)
+     (color-theme-tomorrow-night-eighties)))
+
+(defun my-zoom (n)
+"Increase or decrease font size based upon argument"
+(set-face-attribute 'default (selected-frame) :height
+(+ (face-attribute 'default :height) (* (if (> n 0) 1 -1) 10))))
+(global-set-key (kbd "C-=")      '(lambda nil (interactive) (my-zoom 1)))
+(global-set-key [C-kp-add]       '(lambda nil (interactive) (my-zoom 1)))
+(global-set-key (kbd "C--")      '(lambda nil (interactive) (my-zoom -1)))
+(global-set-key [C-kp-subtract]  '(lambda nil (interactive) (my-zoom -1)))
+(message "All done!")
+
+(add-to-list 'load-path "~/.emacs.d/vendor")
+(require 'puppet-mode)
+(setq auto-mode-alist  (cons '(".pp$" . puppet-mode) auto-mode-alist))
+
+;;; This was installed by package-install.el.
+;;; This provides support for the package system and
+;;; interfacing with ELPA, the package archive.
+;;; Move this code earlier if you want to reference
+;;; packages in your .emacs.
+(when
+    (load
+     (expand-file-name "~/.emacs.d/elpa/package.el"))
+  (package-initialize))
